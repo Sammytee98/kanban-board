4ebacd311b501d4466a8f9fb5fd1dd772ac1ea81
Big Sam <stosin525@gmail.com>
2025-04-15 12:15:09 +0100

    Added dist folder for the build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        home: resolve(__dirname, "home.html"),
+      },
+    },
+  },
 });
